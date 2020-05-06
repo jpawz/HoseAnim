4e3089c14350b51c624012b115c7eccf2737071b
jpawz <jpawz@outlook.com>
2020-05-06 18:47:02 +0200

    added up to 4 parameters log functionality

--- HoseAnim/HoseAnim.cpp
+++ HoseAnim/HoseAnim.cpp
@@ -37,6 +37,14 @@ char parameter_step_l[] = { "Krok:" };
 char parameter_step_v[] = { "step" };
 char cancel[] = { "Cancel" };
 char start[] = { "Start" };
+char measure1[] = { "Pomiar 1:" };
+char measure1Value[] = { "measure1" };
+char measure2[] = { "Pomiar 2:" };
+char measure2Value[] = { "measure2" };
+char measure3[] = { "Pomiar 3:" };
+char measure3Value[] = { "measure3" };
+char measure4[] = { "Pomiar 4:" };
+char measure4Value[] = { "measure4" };
 int window_id;
 
 
@@ -128,6 +136,38 @@ void makeDialogWindow()
 
 	gridOpts.row = 5;
 	gridOpts.column = 1;
+	ProUIDialogLabelAdd(dialogName, measure1, &gridOpts);
+	ProStringToWstring(label[0], measure1);
+	ProUILabelTextSet(dialogName, measure1, label[0]);
+	gridOpts.column = 2;
+	ProUIDialogInputpanelAdd(dialogName, measure1Value, &gridOpts);
+
+	gridOpts.row = 6;
+	gridOpts.column = 1;
+	ProUIDialogLabelAdd(dialogName, measure2, &gridOpts);
+	ProStringToWstring(label[0], measure2);
+	ProUILabelTextSet(dialogName, measure2, label[0]);
+	gridOpts.column = 2;
+	ProUIDialogInputpanelAdd(dialogName, measure2Value, &gridOpts);
+
+	gridOpts.row = 7;
+	gridOpts.column = 1;
+	ProUIDialogLabelAdd(dialogName, measure3, &gridOpts);
+	ProStringToWstring(label[0], measure3);
+	ProUILabelTextSet(dialogName, measure3, label[0]);
+	gridOpts.column = 2;
+	ProUIDialogInputpanelAdd(dialogName, measure3Value, &gridOpts);
+
+	gridOpts.row = 8;
+	gridOpts.column = 1;
+	ProUIDialogLabelAdd(dialogName, measure4, &gridOpts);
+	ProStringToWstring(label[0], measure4);
+	ProUILabelTextSet(dialogName, measure4, label[0]);
+	gridOpts.column = 2;
+	ProUIDialogInputpanelAdd(dialogName, measure4Value, &gridOpts);
+
+	gridOpts.row = 9;
+	gridOpts.column = 1;
 	ProUIDialogPushbuttonAdd(dialogName, cancel, &gridOpts);
 	ProStringToWstring(label[0], cancel);
 	ProUIPushbuttonTextSet(dialogName, cancel, label[0]);
@@ -163,12 +203,29 @@ void startAction(char* dialog, char* component, ProAppData data)
 	wchar_t* paramName;
 	ProUIInputpanelValueGet(dialogName, parameter_v, &paramName);
 
-	ProParameter param;
+	wchar_t* measure1Name;
+	ProUIInputpanelValueGet(dialogName, measure1Value, &measure1Name);
+
+	wchar_t* measure2Name;
+	ProUIInputpanelValueGet(dialogName, measure2Value, &measure2Name);
+
+	wchar_t* measure3Name;
+	ProUIInputpanelValueGet(dialogName, measure3Value, &measure3Name);
+
+	wchar_t* measure4Name;
+	ProUIInputpanelValueGet(dialogName, measure4Value, &measure4Name);
+
+	ProParameter param, param1, param2, param3, param4;
 	ProMdl model;
 	ProModelitem modelItem;
 	ProMdlCurrentGet(&model);
 	ProMdlToModelitem(model, &modelItem);
 	ProError err = ProParameterInit(&modelItem, paramName, &param);
+
+	ProError err1 = ProParameterInit(&modelItem, measure1Name, &param1);
+	ProError err2 = ProParameterInit(&modelItem, measure2Name, &param2);
+	ProError err3 = ProParameterInit(&modelItem, measure3Name, &param3);
+	ProError err4 = ProParameterInit(&modelItem, measure4Name, &param4);
 
 	ProUIMessageButton* buttons;
 	ProUIMessageButton userChoice;
@@ -178,6 +235,9 @@ void startAction(char* dialog, char* component, ProAppData data)
 
 	if (err == PRO_TK_NO_ERROR)
 	{
+		FILE* log;
+		log = fopen("log.csv", "w");
+		fprintf(log, "Param\tm1\tm2\tm3\tm4\n");
 		double initialValue, endValue, stepValue;
 		wchar_t* iValue, *eValue, *sValue;
 		ProUIInputpanelValueGet(dialogName, parameter_init_v, &iValue);
@@ -197,15 +257,45 @@ void startAction(char* dialog, char* component, ProAppData data)
 		ProParamvalue value;
 		value.type = PRO_PARAM_DOUBLE;
 		value.value.d_val = initialValue;
+		ProParamvalue val1;
+		val1.type = PRO_PARAM_DOUBLE;
+		val1.value.d_val = -1;
+		ProParamvalue val2;
+		val2.type = PRO_PARAM_DOUBLE;
+		val2.value.d_val = -1;
+		ProParamvalue val3;
+		val3.type = PRO_PARAM_DOUBLE;
+		val3.value.d_val = -1;
+		ProParamvalue val4;
+		val4.type = PRO_PARAM_DOUBLE;
+		val4.value.d_val = -1;
 		int counter = 0;
 		wchar_t suffix[5];
 
 		while (true) {
 			ProParameterValueSet(&param, &value);
 
+			if (err1 == PRO_TK_NO_ERROR)
+			{
+				ProParameterValueGet(&param1, &val1);
+			}
+			if (err2 == PRO_TK_NO_ERROR)
+			{
+				ProParameterValueGet(&param2, &val2);
+			}
+			if (err3 == PRO_TK_NO_ERROR)
+			{
+				ProParameterValueGet(&param3, &val3);
+			}
+			if (err4 == PRO_TK_NO_ERROR)
+			{
+				ProParameterValueGet(&param4, &val4);
+			}
+
+			ProSolidRegenerate((ProSolid)model, PRO_REGEN_NO_FLAGS);
 			ProSolidRegenerate((ProSolid)model, PRO_REGEN_NO_FLAGS);
 			ProWindowRepaint(window_id);
-
+			fprintf(log, "%f\t%f\t%f\t%f\t%f\n", value.value.d_val, val1.value.d_val, val2.value.d_val, val3.value.d_val, val4.value.d_val);
 			ProDirectoryCurrentGet(output_file);
 			ProWstringConcatenate((wchar_t*)L"image", output_file, 5);
 			swprintf(suffix, L"%05d", counter);
@@ -228,7 +318,7 @@ void startAction(char* dialog, char* component, ProAppData data)
 			counter++;
 		}
 		ProUIMessageDialogDisplay(PROUIMESSAGE_INFO, (wchar_t*)L"Koniec", (wchar_t*)L"Koniec", buttons, PRO_UI_MESSAGE_ABORT, &userChoice);
-
+		fclose(log);
 	}
 	else
 	{
